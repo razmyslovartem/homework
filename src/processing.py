c70ffc12d6fedb402efbe5849c09dea102d83325
razmyslovartem <razmyslovartem@icloud.com>
2025-12-25 14:54:01 +0300

    homework 10_1 done

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,4 @@
-def filter_by_state(operations: list[dict], state: str = 'EXECUTED') -> list[dict]:
+def filter_by_state(operations: list[dict], state: str = "EXECUTED") -> list[dict]:
     """
     Функция принимает список словарей с банковскими операциями.
     Возвращает новый список, содержащий только те словари,
@@ -10,17 +10,17 @@ def filter_by_state(operations: list[dict], state: str = 'EXECUTED') -> list[dic
     """
     filtered_operations = []
     for operation in operations:
-        if operation.get('state') == state:
+        if operation.get("state") == state:
             filtered_operations.append(operation)
     return filtered_operations
 
 
 # Тестирование:
 test_data = [
-    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
 ]
 
 # Тест 1: значение по умолчанию 'EXECUTED'
@@ -29,7 +29,7 @@ print("Результат с state='EXECUTED' (по умолчанию):")
 print(result1)
 
 # Тест 2: явное указание 'CANCELED'
-result2 = filter_by_state(test_data, 'CANCELED')
+result2 = filter_by_state(test_data, "CANCELED")
 print("\nРезультат с state='CANCELED':")
 print(result2)
 
@@ -48,16 +48,16 @@ def sort_by_date(operations: list[dict], reverse: bool = True) -> list[dict]:
 
     # Сортируем по дате
     # Даты уже в ISO формате, который можно сравнивать как строки
-    sorted_operations.sort(key=lambda x: x.get('date', ''), reverse=reverse)
+    sorted_operations.sort(key=lambda x: x.get("date", ""), reverse=reverse)
 
     return sorted_operations
 
 
 test_data = [
-    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
 ]
 
 # Тест 1: сортировка по убыванию (по умолчанию, reverse=True)
